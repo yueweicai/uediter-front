ce76b0c7048dda381ad18504ce9d83ff8f6edf22
yueweicai <weicai.yue@orilore.com>
2018-03-23 21:57:51 +0800

    UEditor 前端项目案例

--- ueditor/ueditor.config.js
+++ ueditor/ueditor.config.js
@@ -3,7 +3,7 @@
     window.UEDITOR_CONFIG = {
         UEDITOR_HOME_URL: URL
         // 服务器统一请求接口路径
-        , serverUrl: "http://localhost:8080/editor/controller.jsp"
+        , serverUrl: "http://localhost:8080/editor/ueditor"
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
             'fullscreen', 'source', '|', 'undo', 'redo', '|',
